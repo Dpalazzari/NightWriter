add44f0462285b8c1be9f8d084b9801e65ffe227
Edilene <edilene_cruz@yahoo.com.br>
2016-10-17 20:40:17 -0600

    updated characters count

--- lib/nightwrite_make_file.rb
+++ lib/nightwrite_make_file.rb
@@ -11,4 +11,4 @@ writer = File.open(ARGV[1], "w")
 writer.write(translated_text)
 writer.close
 
-puts "Created '#{ARGV[1]}' containing #{translated_text.length - 2} characters."
+puts "Created '#{ARGV[1]}' containing #{translated_text.delete("\n").length} characters."
