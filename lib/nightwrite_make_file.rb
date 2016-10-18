e9bc688798f89d65eb29690291d2de6cc53428b2
Drew Palazzari <drewpalazzari@hotmail.com>
2016-10-18 13:02:59 -0600

    library changes

--- lib/nightwrite_make_file.rb
+++ lib/nightwrite_make_file.rb
@@ -11,12 +11,4 @@ writer = File.open(ARGV[1], "w")
 writer.write(translated_text)
 writer.close
 
-puts "Created '#{ARGV[1]}' containing #{translated_text.length - 2} characters."
-
-
-# 
-# 0 => [".0", "00", ".."], 1 => ["0.", "..", ".."],
-# 2 => ["0.", "0.", ".."], 3 => ["00", "..", ".."], 4 => ["00", ".0", ".."],
-# 5 => ["0.", ".0", ".."], 6 => ["00", "0.", ".."], 7 => ["00", "00", ".."],
-# "#" => [".0", ".0", "00"],
-# 8 => ["0.", "00", ".."], 9 => [".0", "0.", ".."]
+puts "Created '#{ARGV[1]}' containing #{translated_text.delete("\n").length} characters."
